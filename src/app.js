8f3f0685a5320ff2b0e0b40429bb165b3c2455c6
hcirlusral <giulianagamba97@gmail.com>
2023-02-27 22:54:49 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,50 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomCard() {
+  const number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = ["♥", "♦", "♠", "♣"];
+
+  let numberIndex = Math.floor(Math.random() * number.length);
+  let numberValue = number[numberIndex];
+
+  let center = document.querySelector("#number");
+
+  center.innerText = numberValue;
+
+  let suitsIndex = Math.floor(Math.random() * suits.length);
+  let suitsValue = suits[suitsIndex];
+
+  let top = document.querySelector("#top");
+  let bottom = document.querySelector("#bottom");
+
+  top.innerText = suitsValue;
+  bottom.innerText = suitsValue;
+
+  if (suitsValue === "♥" || suitsValue === "♦") {
+    top.style.color = "red";
+    bottom.style.color = "red";
+  } else {
+    top.style.color = "black";
+    bottom.style.color = "black";
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCard();
+  setInterval(randomCard, 3000);
 };
